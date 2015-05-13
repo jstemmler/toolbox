d1dfea83a0c7f1c11593fd7a535a76f435266e07
Jayson Stemmler <jstemm@uw.edu>
2015-05-13 11:39:09 -0700

    added setup.py in root directory, moved the modules into sub-directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="toolbox",
+      packages=['toolbox'],
+      version='0.0.1',
+      description="Jayson's Research Toolbox",
+      author="Jayson Stemmler",
+      author_email="jstemm@uw.edu",
+      keywords=['atmos', 'toolbox', 'research', 'science'],
+      classifiers=[],
+)
